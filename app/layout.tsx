b692e180a6f1b1965db8920d27710b58556bde33
Pedro Alencar <pedroalencar.ssr@gmail.com>
2023-07-27 17:23:10 -0300

    feature: finish header animation and intro section

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-36 sm:pt:36`}
       >
         <div
           className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] 
---
+++ components/intro.tsx
@@ -0,0 +1,108 @@
+"use client"
+
+import Image from "next/image"
+import React from "react"
+import { motion } from "framer-motion"
+import Link from "next/link"
+import { BsArrowRight, BsLinkedin } from "react-icons/bs"
+import { HiDownload } from "react-icons/hi"
+import { FaGithubSquare } from "react-icons/fa"
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src="/profile_image.png"
+              alt="Pedro Alencar portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-32 w-32 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text:4x1"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <span className="font-bold">Hi, I'm Pedro Alencar,</span> a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">3 years </span>of experience. I specialize
+        in creating user-friendly web applications with a focus on front-end
+        development. My focus is{" "}
+        <span className="underline">React (Next.js).</span>
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row  items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.1 }}
+      >
+        {/* #Contact me button */}
+        <Link
+          href="#contact"
+          className=" group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
+          focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition "
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        {/* #Download CV button */}
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
+          focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/CV.pdf"
+          download={true}
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        {/* #Redirect to LinkedIn button */}
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full  
+          focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/pedro-alencar-ssr/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        {/* #Redirect to GitHub button */}
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full  
+        focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/alencarPedro"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  )
+}
